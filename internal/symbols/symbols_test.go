de6066bf6ec87828c311e6e2c2ef542939b2e8a5
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-21 20:48:57 -0600

    I need better diagnostics
    The test is failing and I do not know why. I need better
    diagnostics.
    
    The other test was overkill.

--- internal/symbols/symbols_test.go
+++ internal/symbols/symbols_test.go
@@ -9,20 +9,8 @@ func TestSymbolsOnly(t *testing.T) {
 	output := SymbolsOnly(input)
 
 	if expectedOutput != output {
+		t.Logf("expected: %s\n", expectedOutput)
+		t.Logf("actual: %s\n", output)
 		t.Fail()
 	}
-
-	if SymbolsOnly(inputDiscourseOnTheMethod()) != expectedOutputDiscourseOnTheMethod() {
-		t.Fail()
-	}
-}
-
-func inputDiscourseOnTheMethod() string {
-	// excerpt from Discourse On the Method by Rene Descartes
-	// taken from https://www.gutenberg.org/files/59/59-h/59-h.htm, PART I
-	return "Good sense is, of all things among men, the most equally distributed; for every one thinks himself so abundantly provided with it, that those even who are the most difficult to satisfy in everything else, do not usually desire a larger measure of this quality than they already possess."
-}
-
-func expectedOutputDiscourseOnTheMethod() string {
-	return "Goodsenseis,ofallthingsamongmen,themostequallydistributed;foreveryonethinkshimselfsoabundantlyprovidedwithit,thatthoseevenwhoarethemostdifficulttosatisfyineverythingelse,donotusuallydesirealargermeasureofthisquality thantheyalreadypossess."
 }
