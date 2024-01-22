a4e592a285cde242bea060bf7d4c555959410fa3
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-21 19:22:56 -0600

    Implemented dummy fns
    I implemented dummy functions to get the test going, but it doesn't
    want to run. go test still outputs:
    
    \# github.com/gonzalez-nunez-uriel/w100 [github.com/gonzalez-nunez-uriel/w100.test]
    ./main_test.go:21:12: undefined: nill
    FAIL	github.com/gonzalez-nunez-uriel/w100 [build failed]
    
    irrespective of wether I build main or not. I had to implement a
    dummy FormatWidth100 fn and a dummy main fn to get a binary. But
    the test still does not run

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  output := FormatWidth100("dummy string")
+  fmt.Printf("Formating not implemented yet%s\n", output)
+}
+
+func FormatWidth100(input string) string{
+  return ""
+}
