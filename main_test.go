18bcba9647cffbe2ad04ec2279975709b1f6a8b9
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-21 19:13:53 -0600

    Reading tests in testdata
    It does not run using go test. It outputs:
    
    \# github.com/gonzalez-nunez-uriel/w100 [github.com/gonzalez-nunez-uriel/w100.test]
    ./main_test.go:21:12: undefined: nill
    FAIL	github.com/gonzalez-nunez-uriel/w100 [build failed]
    
    It makes sense, since I have not defined the function to test.
    Do I need to run go build to build the test?
    No running go build outputs:
    
    github.com/gonzalez-nunez-uriel/w100: no non-test Go files in /home/haramil/Projects/w100

--- main_test.go
+++ main_test.go
@@ -1,8 +1,33 @@
 package main
 
+import (
+	"os"
+	"testing"
+)
+
 /*
 For this kind of application, it is possible to have (input, output) pairs that are independent of
 implementation.
 */
 
+func TestFormatWidth100(t *testing.T) {
+	validateTests(t)
+}
 
+func validateTests(t *testing.T) {
+
+	testCases, err := os.ReadDir("testdata")
+
+	if err != nill {
+		t.Log("testdata directory could not be read")
+	}
+
+	// For the time being, just print the dir names
+	for _, file := range testCases {
+		t.Log(file.Name())
+	}
+
+	//for each folder in the list append the appropriate file names to open the right data
+
+	//perform verifications on the contents of the files
+}
