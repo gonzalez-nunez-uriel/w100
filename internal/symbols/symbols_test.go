76711d2dd3fbbf6d1be8c4417b93cf1667cecb32
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-21 21:18:01 -0600

    Property based test
    Using strings.Contains to check if there are any offending chars.
    I realized that it would be best to split the tests to get more
    granularity in the diagnostics. That is something that I want to
    do next. I was planning on having values as well as properties
    together just in case one failed, but in this particular case I
    don't think it would be very useful. For that reason I am going to
    delete them in the next commit.

--- internal/symbols/symbols_test.go
+++ internal/symbols/symbols_test.go
@@ -1,12 +1,20 @@
 package symbols
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestSymbolsOnly(t *testing.T) {
 	input := "this is a simple input. Which, contains!\n a lot of $sp chars"
 	expectedOutput := "thisisasimpleinput.Which,contains!alotof$spchars"
 
 	output := SymbolsOnly(input)
+
+	if strings.Contains(output, " ") || strings.Contains(output, "\n") {
+		t.Log("Output contains whitespace or newline chars")
+		t.Fail()
+	}
 
 	if expectedOutput != output {
 		t.Logf("expected: %s\n", expectedOutput)
