0a36d51dad0066518cb644663a03641f80be2ade
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-22 16:42:26 -0600

    Forgot to add test again
    I forgot to add the test again. It says a lot about myself.

--- internal/linematch/linematch_test.go
+++ internal/linematch/linematch_test.go
@@ -19,3 +19,17 @@ func testOne(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func testTwo(t *testing.T) {
+	left := "Another two strings\nthat have a mismatch\nin the first line."
+	right := "Another tw* strings\nthat have a mismatch\nin the first line."
+	//                  ^
+	// mismatch here ---|
+
+	mismatchLineNumber := FindLineMismatch(left, right)
+
+	if mismatchLineNumber != 1 {
+		t.Logf("Failed test one. Expected 1, got %d", mismatchLineNumber)
+		t.Fail()
+	}
+}
