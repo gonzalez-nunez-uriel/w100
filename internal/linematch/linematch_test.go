8e5cb3b58e439442e0699fb971a33f709a2c6cdb
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-22 16:35:12 -0600

    make the mismatch easier to find
    Why is the test passing!?!? It shouldn't pass!

--- internal/linematch/linematch_test.go
+++ internal/linematch/linematch_test.go
@@ -9,6 +9,13 @@ func TestFindLineMismatch(t *testing.T) {
 func testOne(t *testing.T) {
   left := "This two strings\nhave a mismatch\nin the middle line."
   right := "This two strings\nhave @ mismatch\nin the middle line."
+  //                               ^
+  // mismatch here ----------------|
   
+  mismatchLineNumber := FindLineMismatch(left, right)
   
+  if mismatchLineNumber != 2 {
+    t.Logf("Failed test one. Expected 2, got %d", mismatchLineNumber)
+    t.Fail()
+  }
 }
