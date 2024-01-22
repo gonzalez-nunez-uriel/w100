e6ab1beecd38b1bb4cb45b778b62428eed0e8545
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-22 16:37:26 -0600

    Test fails
    That was a dumb mistake. I forgot to call the actual test. I don't
    like this idea that if a function terminates and there is no Fail
    that implies a success. I think it would be better if devs were
    explicit about success. In the event that the test finishes without
    success or failure, then that is a test error.
    
    I also formated the main file.

--- internal/linematch/linematch.go
+++ internal/linematch/linematch.go
@@ -1,19 +1,19 @@
 package linematch
 
 func FindLineMismatch(left string, right string) int {
-  /*linesA := strings.Split(A,"\n")
-  linesB := strings.Split(B,"\n")
-  
-  maxIndexA := len(linesA)
-  maxIndexB := len(linesB)
-  
-  index := 0
-  var lineA string
-  var lineB string
-  for {
-    if 
-    
-  }*/
-  
-  return -1
+	/*linesA := strings.Split(A,"\n")
+	  linesB := strings.Split(B,"\n")
+
+	  maxIndexA := len(linesA)
+	  maxIndexB := len(linesB)
+
+	  index := 0
+	  var lineA string
+	  var lineB string
+	  for {
+	    if
+
+	  }*/
+
+	return -1
 }
--- internal/linematch/linematch_test.go
+++ internal/linematch/linematch_test.go
@@ -3,19 +3,19 @@ package linematch
 import "testing"
 
 func TestFindLineMismatch(t *testing.T) {
-  
+	testOne(t)
 }
 
 func testOne(t *testing.T) {
-  left := "This two strings\nhave a mismatch\nin the middle line."
-  right := "This two strings\nhave @ mismatch\nin the middle line."
-  //                               ^
-  // mismatch here ----------------|
-  
-  mismatchLineNumber := FindLineMismatch(left, right)
-  
-  if mismatchLineNumber != 2 {
-    t.Logf("Failed test one. Expected 2, got %d", mismatchLineNumber)
-    t.Fail()
-  }
+	left := "This two strings\nhave a mismatch\nin the middle line."
+	right := "This two strings\nhave @ mismatch\nin the middle line."
+	//                               ^
+	// mismatch here ----------------|
+
+	mismatchLineNumber := FindLineMismatch(left, right)
+
+	if mismatchLineNumber != 2 {
+		t.Logf("Failed test one. Expected 2, got %d", mismatchLineNumber)
+		t.Fail()
+	}
 }
