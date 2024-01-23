89676f4cdc3bbe6ecf6f753e873e4db6e5f6e015
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-22 18:41:48 -0600

    one string empty test
    Implemented test where one string is empty. As discussed two commits
    ago, the result should be 1. I forgot to think of the case where
    both strings are empty. In that case the two strings are equal and
    the result shoudl be -1.
    
    I have some concerns about strings that do not have a newline
    character. So I am going to make some tests about that.
    
    Adding each test manually to the entry point test is error prone
    and tedious. Isn't there a better way to do this?

--- internal/linematch/linematch_test.go
+++ internal/linematch/linematch_test.go
@@ -4,6 +4,7 @@ import "testing"
 
 func TestFindLineMismatch(t *testing.T) {
 	testIdenticalStrings(t)
+	testOneStringIsEmpty(t)
 	testOne(t)
 	testTwo(t)
 	testThree(t)
@@ -11,7 +12,7 @@ func TestFindLineMismatch(t *testing.T) {
 	testFive(t)
 }
 
-// In case both strings are identical
+// In case both strings are identical, the result should be -1
 func testIdenticalStrings(t *testing.T) {
 	left := "Th3se TWO 5tings @re 1dentical$*!\n\nThe #^% #$() result should\nbe negative 0ne.\n"
 	right := "Th3se TWO 5tings @re 1dentical$*!\n\nThe #^% #$() result should\nbe negative 0ne.\n"
@@ -20,6 +21,19 @@ func testIdenticalStrings(t *testing.T) {
 
 	if mismatchLineNumber != -1 {
 		t.Logf("For two identical strings the output should be -1, got %d", mismatchLineNumber)
+		t.Fail()
+	}
+}
+
+// If one of the strings is empty, the result should be 1
+func testOneStringIsEmpty(t *testing.T) {
+  left := "0ne of th3se %trings is empty.\n\n The result should BE one."
+	right := ""
+	
+	mismatchLineNumber := FindLineMismatch(left, right)
+
+	if mismatchLineNumber != 1 {
+		t.Logf("If one of the strings is empty the result should be 1, got %d", mismatchLineNumber)
 		t.Fail()
 	}
 }
