952ea6fca606fe872f6edecda9cbca78deab6597
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-22 20:09:30 -0600

    Inifite loop
    I got an infinite loop. Not sure how. My logic seems solid. At
    this point I don't want to do print debugging. I don't think it is
    going to help. I guess I have no choice but to set up VS Code.
    
    The reason why I was not using VS Code was because sometimes it
    does a bunch of stuff automatically. What I want is to understand
    what the tools and how my project works. Working with *testing.T
    has me that one more drop is going to overflow the glass. I hate
    it when I don't understand what my code is doing.
    
    So far, I don't like the experience of developing software in Go.
    But I cannot think of any language or software that I have enjoyed.
    I've learned to hate computers.

--- internal/linematch/linematch.go
+++ internal/linematch/linematch.go
@@ -1,5 +1,7 @@
 package linematch
 
+import "strings"
+
 func FindLineMismatch(left string, right string) int {
 	if left == right {
 		return -1
@@ -9,5 +11,37 @@ func FindLineMismatch(left string, right string) int {
 		return 1
 	}
 
-	return -10
+	// After this point both left and right must have at least one line
+
+	leftLines := strings.Split(left, "\n")
+	rightLines := strings.Split(right, "\n")
+
+	maxLeftIndex := len(leftLines) - 1
+	maxRightIndex := len(rightLines) - 1
+
+	index := 0
+	lineNumber := 1
+	var currentLeftLine string
+	var currentRightLine string
+	for {
+		canAccessLeftLines := index <= maxLeftIndex
+		canAccessRightLines := index <= maxRightIndex
+
+		if canAccessLeftLines && canAccessRightLines {
+			currentLeftLine = leftLines[index]
+			currentRightLine = rightLines[index]
+
+			if currentLeftLine != currentRightLine {
+				return lineNumber
+			} else {
+				index += index
+				lineNumber += lineNumber
+			}
+		} else {
+			// One string is longer than the other.
+			// The current line number is where the strings differ
+			// TODO: this test case has not been considered and implemented
+			return lineNumber
+		}
+	}
 }
