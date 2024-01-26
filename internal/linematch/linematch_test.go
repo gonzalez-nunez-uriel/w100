d1919de45cb6d91779b293455c8f060df2c3c300
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-26 04:54:41 -0600

    Midpoint
    I am roughly halfway through the tests. This is tedious but it
    doesn't look that bad. It is not trivial to read the test cases,
    but it is easy to get used to. It does make things clearer.
    
    I am going to have to change the descriptions of some tests. They
    do not sound like a specification, but I have no idea how to 'fix'
    that. I think it is ok, but I am open to other possibilities.
    
    So far so good.

--- internal/linematch/linematch_test.go
+++ internal/linematch/linematch_test.go
@@ -2,6 +2,52 @@ package linematch
 
 import "testing"
 
+type testCase struct {
+	description        string
+	left               string
+	right              string
+	expectedBool       bool
+	expectedLineNumber int
+}
+
+var tests = []testCase{
+	{"Two identical strings have no mismatched lines.",
+		"Th3se TWO 5tings @re 1dentical$*!\n\nThe #^% #$() result should\nbe negative 0ne.\n",
+		"Th3se TWO 5tings @re 1dentical$*!\n\nThe #^% #$() result should\nbe negative 0ne.\n",
+		false,
+		-1},
+	{"If one of the strings is empty the two strings differ at line 1.",
+		"0ne of th3se %trings is empty.\n\n The result should BE one.",
+		"",
+		true,
+		1},
+	{"Failed simple test case.",
+		"These two strings\nhave a mismatch\nin the middle line.",
+		"These two strings\nhave @ mismatch\nin the middle line.",
+		true,
+		2},
+	{"Failed one char mismatch in the first line.",
+		"Another two strings\nthat have a mismatch\nin the first line.",
+		"Another tw* strings\nthat have a mismatch\nin the first line.",
+		true,
+		1},
+	{"Failed one word mismatch at the last line.",
+		"These strings\nhave an empty line\n\nand a mismatch\nin the last line.",
+		"These strings\nhave an empty line\n\nand a mismatch\nin the end.",
+		true,
+		5},
+	{"Failed multiple single char mismatches.",
+		"These strings\nhave more than\n one mismatch.\n\nThe first is in\nthe third line.",
+		"These strings\nhave more than\n 0ne mismatch.\n\nThe first i% in\nthe third line.",
+		true,
+		3},
+	{"Failed multiple word mismatches.",
+		"These strings\nhave more than\n one mismatch.\n\nThe first is in\nthe second line.",
+		"These strings\nhave two\nmismatches.\n\nThe first is in\nthe second line",
+		true,
+		2},
+}
+
 func TestFindLineMismatch(t *testing.T) {
 	testIdenticalStrings(t)
 	testOneStringIsEmpty(t)
