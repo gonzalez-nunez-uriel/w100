79ba16b39970d7c58339d96f6a08ed6ba532dcaf
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-27 19:48:47 -0600

    Confused about license
    I am not sure how this license business works. I have tired to
    learn, but all I find online is vague, contradicting, and always
    followed by 'this is not legal advise' and the like. I understand
    that law matters are delicate, but come on. Just how much do I
    have to learn before I can release an open source software?
    
    I was reading about all of this and I stumbled on a GitHub issue
    where a Contributor License Agreement was mentioned. ... so there
    is a different license agreement for contributions? It was also
    mentioned an AUTHORS file and a CONTRIBUTORS file. The complexity
    of these things is getting out of control. I need to find a
    reliable/concrete learning material to learn about these things.
    Otherwise I am going to be stuff in doubt and stress, and I am
    going to fuck it up anyway.
    
    Right now I am concerned. As I understand, the first comment in a
    file is the description of a package. Then the stuff above the
    package declaration is the rest of the docs for the package.
    Something similar applies to the functions. So I am confused as to
    what is going to happen if I put the license at the top. And I
    can't find information about that specific subject. I can find
    about doc strings in Go and I can find about licenses requiring to
    be placed at the top of every file. It is really frustrating.
    
    I am going to leave it as is because it is the best that I can do
    for now. But I hate this situation. I feel like this all the time
    when I am working with software. I can never be sure of anything.
    Everything is ambiguous, has unlisted caveats, is a prototype, a
    convention, is conflicted by X, Y, and Z. The whole thing is a
    shitshow. Anhd it didn't have to be this way.
    
    I am loosing my SHIT! The autoformater added a # in front of the
    copyright. Why? Queries like "golang what does # mean" do not
    yiled information on the subject. I don't know what to do.
    I don't know how to configure the editor. Nothing.

--- internal/symbols/symbols_test.go
+++ internal/symbols/symbols_test.go
@@ -1,3 +1,26 @@
+/*
+MIT License
+
+# Copyright (c) 2024 Uriel González Núñez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 package symbols
 
 import (
@@ -7,14 +30,14 @@ import (
 
 func TestSymbolsOnly(t *testing.T) {
 
-  input := "\n(~)this is - a 10 simple 124input.\n\n Which, +contains!\n a lot of $sp% &chars"
-  output := SymbolsOnly(input)
-  
+	input := "\n(~)this is - a 10 simple 124input.\n\n Which, +contains!\n a lot of $sp% &chars"
+	output := SymbolsOnly(input)
+
 	if strings.Contains(output, " ") {
 		t.Log("Output contains at least one whitespace character")
 		t.Fail()
 	}
-	
+
 	if strings.Contains(output, "\n") {
 		t.Log("Output contains at least one newline character")
 		t.Fail()
