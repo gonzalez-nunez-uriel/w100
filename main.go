0d464c25c5071f39c6ab203c83782322bf79e841
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-27 20:26:06 -0600

    I don't get it
    I tried to see if it meant anything outside of a comment, but it
    marks it as an error. I don't know what is this thing and it is
    really bothering me.
    
    There is no infor on the subject in https://tip.golang.org/doc/comment.
    I have no clue what to do.

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+//# Copyright (c) 2024 Uriel González Núñez
+# Copyright (c) 2024 Uriel González Núñez
+
 /*
 MIT License
 
@@ -21,6 +24,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+# Copyright (c) 2024 Uriel González Núñez
+
 package main
 
 import "fmt"
