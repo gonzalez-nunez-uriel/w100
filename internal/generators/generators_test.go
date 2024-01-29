411182d4d5ad29c64bbfa88e54be3dd8e10eeb29
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2024-01-28 19:16:19 -0600

    WTH
    I was able to modify the generators.go file and not have a pound
    but that was not working for generators_test.go. I noticed that
    generators.go had a description of the package. So I decided to
    add a comment in generators_test.go and the annoying thing
    disapeared. Is that the reason why there is that automatic formating
    thing? Why does it do this? What does # mean?

--- internal/generators/generators_test.go
+++ internal/generators/generators_test.go
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-# Copyright (c) 2024 Uriel González Núñez
+Copyright (c) 2024 Uriel González Núñez
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the “Software”), to deal in
@@ -21,4 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+// Some comment
 package generators
